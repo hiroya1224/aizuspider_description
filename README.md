6ebd0fcfec5a870e6ae8e3c094d357241faa4f0e
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2019-05-20 21:26:03 +0900

    add send_trajectory_with_pseq.py

---
+++ README.md
@@ -0,0 +1,1 @@
+./send_trajectory_with_pseq.py -F aizu_p0.pseq --offset=2
---
+++ send_trajectory_with_pseq.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+
+from control_msgs.msg import (
+        FollowJointTrajectoryAction,
+        FollowJointTrajectoryGoal,
+    )
+from trajectory_msgs.msg import (
+        JointTrajectoryPoint,
+    )
+
+import yaml
+import argparse
+
+class SingleTrajectory:
+    def __init__(self, namespace='AizuSpider'):
+        self.act_ = actionlib.SimpleActionClient(
+            '%s/fullbody_controller/follow_joint_trajectory'%(namespace),
+            FollowJointTrajectoryAction,
+        )
+        self.act_.wait_for_server(rospy.Duration(10.0))
+    def feedback(self, msg):
+        pass
+
+    def call(self, ref_list, offset = 0.0):
+        '''ref_list = [ [tm, joints, q], ... ]'''
+        goal = FollowJointTrajectoryGoal()
+
+        goal.trajectory.joint_names = ['FR_FLIPPER', 'FL_FLIPPER', 'BR_FLIPPER', 'BL_FLIPPER',
+                                       'SHOULDER', 'ARM', 'FOREARM', 'WRIST1', 'WRIST2', 'HAND',
+                                       'FINGER1', 'FINGER2', 'FINGER3', ]
+        goal.trajectory.points = []
+        for ref in ref_list:
+            p = JointTrajectoryPoint()
+            p.time_from_start = rospy.Duration(ref[0] + offset)
+            qlst = []
+            q = ref[2]
+            for j in ref[1]:
+                qlst.append(q[j])
+            p.positions = qlst
+            goal.trajectory.points.append(p)
+
+        goal.trajectory.header.stamp = rospy.Time.now() + rospy.Duration(0.01)
+
+        print(goal.trajectory)
+        self.act_.send_goal(goal, feedback_cb=self.feedback)
+
+    def wait(self, tm=rospy.Duration(10)):
+        self.act_.wait_for_result(tm)
+
+# index_map = {
+#     0: 'FR_FLIPPER',
+#     1: 'FL_FLIPPER',
+#     2: 'BR_FLIPPER',
+#     3: 'BL_FLIPPER',
+#     4: 'SHOULDER',
+#     5: 'ARM',
+#     6: 'FOREARM',
+#     7: 'WRIST1',
+#     8: 'WRIST2',
+#     9: 'HAND',
+#     10: 'FINGER1',
+#     11: 'FINGER2',
+#     12: 'FINGER3',
+#     }
+index_lst = [
+    'FR_FLIPPER',
+    'FL_FLIPPER',
+    'BR_FLIPPER',
+    'BL_FLIPPER',
+    'SHOULDER',
+    'ARM',
+    'FOREARM',
+    'WRIST1',
+    'WRIST2',
+    'HAND',
+    'FINGER1',
+    'FINGER2',
+    'FINGER3',
+    ]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='call trajectory action by PoseSeq')
+    parser.add_argument('-N', '--name', default = None, type=str)
+    parser.add_argument('-F', '--file_name', default = None, type=str)
+    parser.add_argument('--offset',     default = None, type=str)
+    parser.add_argument('--wait',       default = None, type=str)
+
+    rospy.init_node('call_from_pseq', anonymous=True)
+
+    args = parser.parse_args()
+
+    name  = args.name
+    if not name:
+        name = 'AizuSpider'
+
+    filename  = args.file_name
+    if not filename:
+        print('PoseSeq file required with -F option')
+        exit(1)
+
+    offset = args.offset
+    if not offset:
+        offset = 0.0
+    else:
+        offset = float(offset)
+
+    wait_tm = args.wait
+    if not wait_tm:
+        wait_tm = 1.0
+    else:
+        wait_tm = float(wait_tm)
+
+    f = open(filename, 'r')
+    base_pseq = yaml.load(f)
+    pseq = base_pseq['refs']
+
+    ref_lst = []
+    for p in pseq:
+        ref = p['refer']
+        tm =  p['time']
+        js = ref['joints']
+        q  = ref['q']
+        ##print tm,js,q
+        ref_lst.append([tm, js, q])
+
+    sg = SingleTrajectory(namespace=name)
+
+    sg.call(ref_lst, offset=offset)
+    sg.wait(rospy.Duration(wait_tm))
